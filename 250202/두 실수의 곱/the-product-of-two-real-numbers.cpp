43907cd39d4ce8bf6830c974b4d0e60e7f200051
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-02-02 17:34:07 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+
+    double a = 5.26;
+    double b = 8.27;
+    double c = a * b;
+
+    cout.precision(3);
+    cout << c << endl;
+    return 0;
+}
