fa24ca99e0c97b2b733595de2c139b51e76ad66e
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-02-03 23:14:12 +0900

    Title: 데이터 교환, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/데이터 교환/exchange-data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6, c = 7;
+    int temp;
+
+    temp = c;
+    c = b;
+    b = a;
+    a = temp;
+
+    cout << a << endl << b << endl << c << endl;
+    return 0;
+}
