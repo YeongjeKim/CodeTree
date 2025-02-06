01c33f242719adb5965b5a13e312d4b8bb2be2c0
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-02-06 22:59:36 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250206/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 010;
+    int x, y;
+    cin >> a;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+    cout << "010-" << y << "-" << x << endl;
+    return 0;
+}
