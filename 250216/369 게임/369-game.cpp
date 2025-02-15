60ec211dfdda26c6608c48d35e3a1493062ed309
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-02-16 00:07:15 +0900

    Title: 369 게임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250216/369 게임/369-game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        int a = i / 10;
+        int b = i % 10; 
+
+        if (a == 3 || a == 6 || a == 9 || b == 3 || b == 6 || b == 9 || i % 3 == 0) {
+            cout << 0 << " ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
