f0fcf1e01bb4efb85d3dcd44f04e121a571ff4a5
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-02-23 15:37:02 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250223/별표 출력하기 10/print-star-10.cpp
+++ 250223/별표 출력하기 10/print-star-10.cpp
@@ -21,12 +21,12 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
-            for (int j = 0; j < (n + 1) / 2 - i / 2; j++) {
+            for (int j = 0; j < ((n + 1) / 2) - (i / 2); j++) {
                 cout << "* ";
             }
         }
         else {
-            for (int j = 0; j < (n + 1) / 2 + (i + 1) / 2; j++) {
+            for (int j = 0; j < ((n + 1) / 2) + ((i + 1) / 2); j++) {
                 cout << "* ";
             }
         }
