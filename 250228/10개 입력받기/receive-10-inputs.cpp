2c175519b3dc53bea2a7ff77b4f19444e3b1ded3
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-02-28 23:42:34 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250228/10개 입력받기/receive-10-inputs.cpp
+++ 250228/10개 입력받기/receive-10-inputs.cpp
@@ -3,28 +3,23 @@ using namespace std;
 
 int main() {
     int arr[10];
-    int sum = 0;
-    int stop = 0;
     
     for (int i = 0; i < 10; i++) {
         cin >> arr[i];
     }
 
+    int sum = 0, cnt = 0;
+
     for (int i = 0; i < 10; i++) {
-        if (arr[i] == 0) {
-            stop = i;
-            break;
-        }
+        if (arr[i] == 0) break;
+        sum += arr[i];
+        cnt++;
     }
 
-    int j = 0;
-    while (j < stop) {
-        sum += arr[j];
-        j++;
-    }
-
+    double avg = double(sum) / cnt;
     cout << fixed;
     cout.precision(1);
-    cout << sum << " " << double(sum) / stop << endl;
+    cout << sum << " " << avg << endl;
+
     return 0;
 }
