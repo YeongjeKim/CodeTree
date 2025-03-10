74d50ad083f86952e7924032a7cd15a19183e76a
Yeongje Kim <52230134+YeongjeKim@users.noreply.github.com>
2025-03-10 22:50:03 +0900

    Title: 배열의 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250310/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[2][4];
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << fixed; cout.precision(1);
+
+    for (int i = 0; i < 2; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < 4; j++) {
+            sum += arr[i][j];
+        }
+        cout << sum / 4 << " ";
+    }
+    cout << endl;
+
+    for (int j = 0; j < 4; j++) {
+        double sum = 0.0;
+        for (int i = 0; i < 2; i++) {
+            sum += arr[i][j];
+        }
+        cout << sum / 2 << " ";
+    }
+    cout << endl;
+
+    double sum = 0.0;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            sum += arr[i][j];
+        }
+    }
+    cout << sum / 8 << endl;
+
+    return 0;
+}
